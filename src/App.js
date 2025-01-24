f42da09bce06ee23156966a901fc01c51f588d71
Srishti Chordia <117989279+srishti-chordia@users.noreply.github.com>
2025-01-25 00:51:18 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import DoctorLogin from './styles/DoctorLogin'; // Import your DoctorLogin component
 import './styles/App.scss';
 
 function App() {
   return (
     <Router>
+      <nav>
+        {/* Add a link to the Doctor Login page */}
+        <Link to="/doctor-login">Doctor Login</Link> 
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/doctor-login" element={<DoctorLogin />} /> 
       </Routes>
     </Router>
   );
